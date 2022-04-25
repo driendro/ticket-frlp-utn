7b3e7236ed633b0f2554cd81eb976eea1093691b
Driendro <jorge.ronconi@gmail.com>
2022-04-24 22:50:41 -0300

    creacion del login y index del vendedor

---
+++ application/controllers/LoginVendedor.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class LoginVendedor extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('vendedor_model');
+
+		// Esta funcion controla si existe la variable de sesion 'is_admin' para
+		// impedir que muestre el formulario de ingreso si ya esta logeado
+		if($this->session->userdata('is_admin'))
+		{
+			redirect(base_url('admin'));
+		}
+
+    }
+
+
+	public function Login()
+    {
+        if($this->session->userdata('is_admin'))
+        {
+			redirect(base_url('admin/index'));
+        }
+		if($this->session->userdata('is_user'))
+		{
+			redirect(base_url('logout'));
+		}
+
+        $data = [
+            'titulo' => 'Login Vendedor'
+        ];
+
+        if ($this->input->method() == 'post')
+        {
+            $nickName = $this->input->post('nick-name');
+            $password = $this->input->post('password');
+			$nickName = strtolower($nickName);
+			$documento = $this->vendedor_model->getDocumentoByNickName($nickName);
+
+            if($this->vendedor_model->validateVendedor($nickName, md5($password)))
+            {
+                $session = [
+                    'id_vendedor'  => $this->vendedor_model->getIdByDocumento($documento),
+					'nick_name' => $nickName,
+                    'apellido' => $this->vendedor_model->getLastNameByDocumento($documento),
+                    'nombre' => $this->vendedor_model->getFirstNameByDocumento($documento),
+					'is_user'=> FALSE,
+					'is_admin'=> TRUE
+                ];
+                $this->session->set_userdata($session);
+                redirect(base_url('admin/index'));
+            }
+            else
+            {
+                redirect(base_url('admin'));
+            }
+        }
+        else
+        {
+            $this->load->view('vendedor/header', $data);
+            $this->load->view('vendedor/login');
+            $this->load->view('footer');
+        }
+    }
+}
---
+++ application/views/vendedor/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?= base_url('assets/css/style.css'); ?>">
+	<title><?= $titulo; ?></title>
+</head>
+
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="<?= base_url('ticket'); ?>">Ticket</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader"
+				aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarHeader">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="<?= base_url('menu'); ?>">Menu</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?= base_url('contacto'); ?>">Contacto</a>
+					</li>
+					<?php if($this->session->userdata('id_vendedor')): ?>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+							data-bs-toggle="dropdown"
+							aria-expanded="false"><?= $this->session->userdata('apellido'); ?>,
+							<?= $this->session->userdata('nombre'); ?></a>
+						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<li>
+								<a class="dropdown-item" href="<?= base_url('usuario/historial'); ?>">Historial de
+									compras</a>
+							</li>
+							<li>
+								<a class="dropdown-item" href="<?= base_url('usuario/cambio-password'); ?>">Cambiar
+									contraseña</a>
+							</li>
+							<li>
+								<hr class="dropdown-divider">
+							</li>
+							<li>
+								<a class="dropdown-item" href="<?= base_url('logout'); ?>">Logout</a>
+							</li>
+						</ul>
+					</li>
+					<?php endif; ?>
+				</ul>
+			</div>
+		</div>
+	</nav>
