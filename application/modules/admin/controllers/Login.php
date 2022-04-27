fb8f378600ef1f541fb5f20ae36e58405c85219b
Walter Pagani <paganiwalter@gmail.com>
2022-04-27 16:38:05 -0300

    Moviendo los ficheros dentro del modulo admin (#6)

--- application/modules/admin/controllers/Login.php
+++ application/modules/admin/controllers/Login.php
@@ -1,34 +1,25 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class LoginVendedor extends CI_Controller
+class Login extends MX_Controller
 {
     public function __construct()
     {
         parent::__construct();
-
         $this->load->model('vendedor_model');
-
-		// Esta funcion controla si existe la variable de sesion 'is_admin' para
-		// impedir que muestre el formulario de ingreso si ya esta logeado
-		// if($this->session->userdata('is_admin'))
-		// {
-		// 	redirect(base_url('admin/index'));
-		// }
-
     }
 
-
-	public function Login()
+    public function index()
     {
         if($this->session->userdata('is_admin'))
         {
-			redirect(base_url('admin/index'));
+            redirect(base_url('admin'));
         }
-		if($this->session->userdata('is_user'))
-		{
-			redirect(base_url('logout'));
-		}
+
+        if($this->session->userdata('is_user'))
+        {
+            redirect(base_url('logout'));
+        }
 
         $data = [
             'titulo' => 'Login Vendedor'
@@ -38,25 +29,26 @@ class LoginVendedor extends CI_Controller
         {
             $nickName = $this->input->post('nick-name');
             $password = $this->input->post('password');
-			$nickName = strtolower($nickName);
-			$documento = $this->vendedor_model->getDocumentoByNickName($nickName);
+            $nickName = strtolower($nickName);
+            $documento = $this->vendedor_model->getDocumentoByNickName($nickName);
 
             if($this->vendedor_model->validateVendedor($nickName, md5($password)))
             {
                 $session = [
                     'id_vendedor'  => $this->vendedor_model->getIdByDocumento($documento),
-					'nick_name' => $nickName,
+                    'nick_name' => $nickName,
                     'apellido' => $this->vendedor_model->getLastNameByDocumento($documento),
                     'nombre' => $this->vendedor_model->getFirstNameByDocumento($documento),
-					'is_user'=> FALSE,
-					'is_admin'=> TRUE
+                    'is_user'=> FALSE,
+                    'is_admin'=> TRUE
                 ];
+
                 $this->session->set_userdata($session);
-                redirect(base_url('admin/index'));
+                redirect(base_url('admin'));
             }
             else
             {
-                redirect(base_url('admin'));
+                redirect(base_url('admin/login'));
             }
         }
         else
