68ba007190dad34b3bdd6a2d61a582eb22afc6cd
Driendro <jorge.ronconi@gmail.com>
2022-05-18 22:07:26 -0300

    footer

--- application/modules/general/views/footer.php
+++ application/modules/general/views/footer.php
@@ -2,18 +2,22 @@
 	<div class="container">
 		<div class="row">
 			<div class="col">
-				<p class="text-center mb-0">Este sistema fue desarrollado por <strong>Lautaro Bifano</strong>,
-					<strong>Dario Rumbo</strong> y <strong>Jose Dorce</strong>.
+				<p class="text-center mb-0">Desarrollado por <strong>RONCONI, Jorge</strong> y <strong>PAGANI,
+						Walter</strong>.</p>
+				<p class="text-center mb-0">Basado en el sistema desarrollado por <strong>Lautaro
+						Bifano</strong>, <strong>Dario Rumbo</strong> y <strong>Jose Dorce</strong>.</p>
+				<p class="text-center mb-0">Actualizado por <strong>Secretaria de TIC </strong> -
+					<strong>UTN-FRLP</strong>.
 				</p>
-				<p class="text-center mb-0">Actualmente es actualizado por el gran <strong>Ronconi Jorge</strong> y
-					el inigualable <strong>Pagani Walter Horacio</strong>.</p>
 				<p class="text-center mb-0"><strong>Ticket-Web © Copyright <?= date('Y'); ?></strong></p>
 			</div>
 		</div>
 	</div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+	integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+	integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
 <script src="<?= base_url('assets/js/vue.js'); ?>"></script>
 </body>
