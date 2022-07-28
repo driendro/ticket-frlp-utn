af9c94bc6a8f89164db6a5017b519ec1a351ca02
Driendro <jorge.ronconi@gmail.com>
2022-07-28 12:35:07 -0300

    refact comedor/ticket controller

--- application/modules/comedor/models/Ticket_model.php
+++ application/modules/comedor/models/Ticket_model.php
@@ -10,6 +10,9 @@ class Ticket_model extends CI_Model
 
     public function addCompra($data)
     {
+        /* Usado en:
+        compra
+        */
         if ($this->db->insert('compra', $data)) {
             return true;
         } else {
@@ -19,6 +22,10 @@ class Ticket_model extends CI_Model
 
     public function addLogCompra($data)
     {
+        /* Usado en:
+        compra
+        devolverCompra
+        */
         if ($this->db->insert('log_compra', $data)) {
             return true;
         } else {
@@ -28,6 +35,10 @@ class Ticket_model extends CI_Model
 
     public function updateSaldoByIDUser($id_user, $saldo_nuevo)
     {
+        /* Usado en:
+        compra
+        devolverCompra
+        */
         $this->db->set('saldo', $saldo_nuevo);
         $this->db->where('id', $id_user);
         if ($this->db->update('usuarios')) {
@@ -37,22 +48,36 @@ class Ticket_model extends CI_Model
         }
     }
 
-    public function getCostoById($id)
+    public function getCostoByID($id)
     {
-        $idPrecio = $this->db->select('id_precio')->where('id', $id)->get('usuarios')->row('id_precio');
-        return $this->db->select('costo')->where('id', $idPrecio)->get('precios')->row('costo');
+        /* Usado en:
+        index
+        compra
+        devolverCompra
+        */
+        $this->db->select('costo');
+        $this->db->where('id', $id);
+        $query = $this->db->get('precios');
+        return $query->row('costo');
     }
 
     public function getCompraById($id)
     {
+        /*Usado en:
+        devolverCompra
+        */
         $this->db->select('*');
         $this->db->where('id', $id);
         $query = $this->db->get('compra');
-        return $query->result();
+        return $query->row();
     }
 
     public function getSaldoByIDUser($id_user)
     {
+        /*Usado en:
+        compra
+        devolverCompra
+        */
         $this->db->select('saldo');
         $this->db->where('id', $id_user);
         $query = $this->db->get('usuarios');
@@ -61,6 +86,10 @@ class Ticket_model extends CI_Model
 
     public function getComprasInRangeByIdUser($fecha_i, $fecha_f, $idusuario)
     {
+        /* Usado en:
+        index
+        devolverCompra
+        */
         $this->db->select('*');
         $this->db->where('id_usuario', $idusuario);
         $this->db->where('dia_comprado >=', $fecha_i);
@@ -72,6 +101,9 @@ class Ticket_model extends CI_Model
 
     public function getComprasByIDTransaccion($id_trans)
     {
+        /* Usado en:
+        compra
+        */
         $this->db->select('*');
         $this->db->where('transaccion_id', $id_trans);
         $query = $this->db->get('compra');
@@ -80,6 +112,10 @@ class Ticket_model extends CI_Model
 
     public function getLogComprasByIDTransaccion($id_trans)
     {
+        /* Usado en:
+        compra
+        devolverCompra
+        */
         $this->db->select('*');
         $this->db->where('transaccion_id', $id_trans);
         $query = $this->db->get('log_compra');
@@ -88,12 +124,19 @@ class Ticket_model extends CI_Model
 
     public function addTransaccion($data)
     {
+        /* Usado en:
+        compra
+        devolverCompra
+        */
         $this->db->insert('transacciones', $data);
         return $this->db->insert_id();
     }
 
     public function updateTransactionInCompraByID($id_compra, $id_trans)
     {
+        /* Usado en:
+        compra
+        */
         $this->db->set('transaccion_id', $id_trans);
         $this->db->where('id', $id_compra);
         $this->db->update('compra');
@@ -101,24 +144,36 @@ class Ticket_model extends CI_Model
 
     public function updateTransactionInLogCompraByID($id_compra, $id_trans)
     {
+        /* Usado en:
+        compra
+        devolverCompra
+        */
         $this->db->set('transaccion_id', $id_trans);
         $this->db->where('id', $id_compra);
         $this->db->update('log_compra');
     }
 
-    public function updateTransactionInCargaByID($id_carga, $id_trans)
-    {
-        $this->db->set('transaccion_id', $id_trans);
-        $this->db->where('id', $id_carga);
-        $this->db->update('log_carga');
-    }
-
     public function removeCompra($idcompra)
     {
+        /* Usado en:
+        devolverCompra
+        */
         if ($this->db->delete('compra', ['id' => $idcompra])) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public function getFeriadosInRange($fecha_i, $fecha_f)
+    {
+        /* Usado en:
+        index
+        */
+        $this->db->select('*');
+        $this->db->where('fecha >=', $fecha_i);
+        $this->db->where('fecha <=', $fecha_f);
+        $query = $this->db->get('feriado');
+        return $query->result();
     }
 }
